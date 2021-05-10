db082cbfa9b95f569c7b75fc11416d50a6992c36
Chapadox <ffcxcfsantos2@gmail.com>
2021-05-10 19:33:10 -0300

    🐛 Resolvendo Bugs e Adicionando um novo sistema de pesquisa

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,25 +1,35 @@
 const url = 'http://localhost:3000/'
 
 // selecting items
-const buttonSearch= document.getElementById('botao')
+const buttonSearch = document.getElementById('botao')
 const inputSearch = document.getElementById('pesquisar')
-const dv = document.getElementById('main')
+const main = document.getElementById('main')
+const persoImg = document.getElementById('image')
+const testediv = document.getElementById('teste')
+// 
+
+// "New Page"
+const desc = document.getElementById('description')
+const img  = document.getElementById('img') 
 // 
 
 async function get() {
   const getUrl = await fetch('http://localhost:3000')
   const data = await getUrl.json()
   console.log(data)
-  showData(data)
+  search(data)
 }
 
-function showData(dado) {
-  if (inputSearch.value == dado[0].nome) {
-    inputSearch.value = ''
-    main.innerHTML = ''
-
-  } else {
-    alert('Desculpa, Pokémon não encontrado. ')
+function search(dado) {
+  for (var i = 0; i < dado.length; i++) {
+    if (dado[i].nome === inputSearch.value) {
+      inputSearch.value = ''
+      main.innerHTML = ''
+      desc.innerHTML = dado[i].descrição
+      img.src = dado[i].imagem
+    }
   }
 }
-buttonSearch.addEventListener('click', get) 
+
+
+buttonSearch.addEventListener('click', get)
